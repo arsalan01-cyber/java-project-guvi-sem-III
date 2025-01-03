1827e4d97ef0c9ff73ab734f31722b677abccb77
Md Arsalan Alam <arsalanalam160@gmail.com>
2025-01-03 15:50:43 +0530

    Add files via upload

---
+++ registration and profile Servlets./htmlForm.java
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+    <form action="/quizgame/register" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <input type="submit" value="Register">
+    </form>
+    <br>
+    <a href="/quizgame/login">Already have an account? Log in</a>
+</body>
+</html>
