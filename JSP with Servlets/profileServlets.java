16301aea8e21fba6df43e6d32bde920465da3ce8
Md Arsalan Alam <arsalanalam160@gmail.com>
2025-01-03 16:31:21 +0530

    Add files via upload

---
+++ JSP with Servlets/profileServlets.java
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Profile</title>
+</head>
+<body>
+    <h2>Welcome, ${user.username}</h2>
+    <p>Email: ${user.email}</p>
+    <br>
+    <a href="quiz.jsp">Start Quiz</a><br>
+    <a href="logout">Logout</a>
+</body>
+</html>
