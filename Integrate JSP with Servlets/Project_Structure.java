b46c0203d44d7a0812e23c37544250ee162a6181
Md Arsalan Alam <arsalanalam160@gmail.com>
2025-01-03 16:45:33 +0530

    Add files via upload

---
+++ Integrate JSP with Servlets/Project_Structure.java
@@ -0,0 +1,16 @@
+quiz-game/
+├── WEB-INF/
+│   ├── classes/
+│   │   └── com/quizgame/servlet/
+│   │       ├── RegistrationServlet.java
+│   │       ├── LoginServlet.java
+│   │       ├── QuizServlet.java
+│   │       ├── ProfileServlet.java
+│   │       └── LogoutServlet.java
+│   ├── lib/
+│   └── web.xml
+├── index.jsp
+├── register.jsp
+├── login.jsp
+├── profile.jsp
+└── quiz.jsp
