233212d8a61e6545723ca285274695f6533ea395
Md Arsalan Alam <arsalanalam160@gmail.com>
2025-01-03 23:35:10 +0530

    Add files via upload

---
+++ Controller logic implementation/model.java
@@ -0,0 +1,45 @@
+// Student.java (Model)
+
+public class model {
+    private int id;
+    private String name;
+    private String email;
+
+    // Constructor
+    public model(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // toString Method for easy display
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "', email='" + email + "'}";
+    }
+}
