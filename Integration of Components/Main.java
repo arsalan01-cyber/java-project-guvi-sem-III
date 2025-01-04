da4f09a69c36d39b80451bebaf9d2802dbd9ce78
Md Arsalan Alam <arsalanalam160@gmail.com>
2025-01-04 09:18:30 +0530

    Add files via upload

---
+++ Integration of Components/Main.java
@@ -0,0 +1,10 @@
+package com.library;
+
+import com.library.ui.LibraryUI;
+
+public class Main {
+    public static void main(String[] args) {
+        LibraryUI libraryUI = new LibraryUI();
+        libraryUI.start();
+    }
+}
